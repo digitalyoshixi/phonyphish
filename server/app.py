5b88e6f8962fc15a06aaa76cb1c5073498d0a19f
yoshixi <digitalyoshixi@proton.me>
2025-01-18 07:39:22 -0500

    testing for voice endpoint

--- server/app.py
+++ server/app.py
@@ -1,10 +1,37 @@
-from flask import Flask 
+from flask import Flask, request, Response
+from flask_sock import Sock 
+from twilio.twiml.voice_response import VoiceResponse
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__) # designates this script as the root apth
+sock = Sock(app)
+CORS(app, supports_credentials=True, origins="*")
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "hello world"
+    if request.method == "POST":
+        xml = f"""
+        <Reponse>
+            <Say>
+                You have connected to flask
+            </Say>
+        </Reponse>
+        """.strip()
+        return Response(xml, mimetype='text/xml')
+    else:
+        return "hello world"
+
+@sock.route('/realtime')
+def transcription_websocket(ws):
+    pass
+
+#@app.route("/answer", methods=['GET', 'POST'])
+#def answer_call():
+#    resp = VoiceResponse()
+#    resp.say("Thank you for calling! Have a great day.", voice='Polly.Amy')
+#    return str(resp)
 
 if __name__ == "__main__": # if running this file directly
-    app.run(debug=True) # run the app
+    app.run(host='0.0.0.0', port=8000, debug=True) # run the app
--- server/calling.py
+++ server/calling.py
@@ -11,14 +11,12 @@ account_sid = os.getenv("TWILIO_SID")
 auth_token = os.getenv("TWILIO_AUTH")
 client = Client(account_sid, auth_token)
 
-print(account_sid)
-print(auth_token)
-
-
-call = client.calls.create(
-  url="http://demo.twilio.com/docs/voice.xml",
-  to="+14168797536",
-  from_="+16282031965"
-)
-
-print(call.sid)
+# send call
+def sendcall():
+  call = client.calls.create(
+    url="http://demo.twilio.com/docs/voice.xml",
+    to="+14168797536",
+    from_="+16282031965"
+  )
+  print(call.sid)
+  
