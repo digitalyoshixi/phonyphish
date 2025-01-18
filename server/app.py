96b96b9278ea0579a010e7fc34216846bcb9bc67
yoshixi <digitalyoshixi@proton.me>
2025-01-18 10:50:43 -0500

    session ok?

--- server/app.py
+++ server/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, Response
-#from flask_sock import Sock 
+from flask_sock import Sock 
 from twilio.twiml.voice_response import VoiceResponse, Connect, Stream
 from flask_cors import CORS, cross_origin
-from flask_sockets import Sockets
+#from flask_sockets import Sockets
 import json 
 from datetime import datetime
 
 app = Flask(__name__) # designates this script as the root apth
-sockets = Sockets(app)
+sock= Sock(app)
 CORS(app, supports_credentials=True, origins="*")
 app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -20,19 +20,27 @@ def index():
         response.say("Helo, the call is connected go speak now.", voice="woman")
         # Start recording with transcription enabled
         connect = Connect()
-        connect.stream(url='wss://ec2-54-234-22-87.compute-1.amazonaws.com:8000/transcription_callback')
+        connect.stream(url=f'wss://{request.host}/transcription_callback')
         response.append(connect)
         return str(response)
     else:
         return "hi this is transcription testing, dont view this with a GET request"
 
-@app.route("/transcription_callback", methods=['POST'])
-def handle_transcription():
-    """Handle the transcription callback from Twilio."""
-    # Get the transcription data
-    print("hi guys")
-    return 'OK'
-
+@sock.route('/transcription_callback')
+def transcription_callback(ws):
+    while True:
+        data = json.loads(ws.recieve())
+        type = data['event']
+        if type == "connected":
+            print("connected")  
+        if type == "start":
+            print("start")  
+        if type == "media":
+            print("media")  
+            payload = data['media']['payload']
+            print(payload)
+        if type == "stop":
+            print("stop")  
 
 
 if __name__ == "__main__": # if running this file directly
