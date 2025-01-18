82e3b51042c77b1a0d6bf806edcc808384da06c9
yoshixi <digitalyoshixi@proton.me>
2025-01-18 08:43:41 -0500

    pls have stream work

--- server/app.py
+++ server/app.py
@@ -15,7 +15,7 @@ def index():
     if request.method == "POST":
         xml = f"""
 <?xml version="1.0" encoding="UTF-8"?>
-<Response><Say voice="woman">Helo the call is connected go speak now</Say><Connect></Connect></Response>
+<Response><Say voice="woman">Helo the call is connected go speak now</Say><Connect><Stream url='wss://{request.host}/realtime'/></Connect></Response>
         """.strip()
         print(xml)
         return Response(xml, mimetype='text/xml')
