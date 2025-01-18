8bdd1b4c7f09bcbf909f9f80ebc015d0579aa8c0
yoshixi <digitalyoshixi@proton.me>
2025-01-18 14:45:52 -0500

    voice recordings

---
+++ server/viewtranscriptions.py
@@ -0,0 +1,17 @@
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+# Set environment variables for your credentials
+# Read more at http://twil.io/secure
+
+account_sid = os.getenv("TWILIO_SID")
+auth_token = os.getenv("TWILIO_AUTH")
+client = Client(account_sid, auth_token)
+
+# Fetch the latest transcription
+transcriptions = client.transcriptions.list(limit=1)
+if transcriptions:
+    latest_transcription = transcriptions[0]
+    print(latest_transcription.transcription_text)
