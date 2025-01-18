2d27c3e5b9dc5a2c19f4055f1696aa68ab8e3283
yoshixi <digitalyoshixi@proton.me>
2025-01-18 15:59:02 -0500

    pwa changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+const withPWA = require('next-pwa');
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,      // Enable React strict mode for improved error handling
+  swcMinify: true,            // Enable SWC minification for improved performance
+  compiler: {
+        removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
+    }
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",         // destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
+  register: true,         // register the PWA service worker
+  skipWaiting: true,      // skip waiting for service worker activation
+})(nextConfig);
+
