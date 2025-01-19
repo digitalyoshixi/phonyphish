35afcb8b33c7dfb9bf19fc1c8828149de28a113f
rizfol <blahblabh@example.com>
2025-01-18 20:44:52 -0500

    turned invoke into func

--- server/invokeEndpoint.py
+++ server/invokeEndpoint.py
@@ -20,18 +20,19 @@ sagemaker_runtime = boto3.client(
 # Specify the endpoint name
 endpoint_name = 'huggingface-pytorch-inference-2025-01-18-21-54-37-838'
 
-# Prepare the payload for the request (replace with your actual data)
-payload = json.dumps({"inputs": "test test test"})
+def invoke_endpoint(scam_text):
+    # Prepare the payload for the request (replace with your actual data)
+    payload = json.dumps({"inputs": f'{scam_text}'})
 
-try:
-    # Call the SageMaker endpoint
-    response = sagemaker_runtime.invoke_endpoint(
-        EndpointName=endpoint_name,
-        ContentType='application/json',  # Specify content type (for example, JSON)
-        Body=payload  # Send your input data
-    )
-    # Process the response
-    result = json.loads(response['Body'].read().decode())
-    print(result)
-except boto3.exceptions.Boto3Error as e:
-    print(f"An error occurred: {e}")
+    try:
+        # Call the SageMaker endpoint
+        response = sagemaker_runtime.invoke_endpoint(
+            EndpointName=endpoint_name,
+            ContentType='application/json',  # Specify content type (for example, JSON)
+            Body=payload  # Send your input data
+        )
+        # Process the response
+        result = json.loads(response['Body'].read().decode())
+        print(result)
+    except boto3.exceptions.Boto3Error as e:
+        print(f"An error occurred: {e}")
